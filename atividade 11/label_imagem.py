bfe5b5effcc9d664e168c14d3d3288a71b15affa
“AngelaSIQ” <angela.siqueira@escolar.ifrn.edu.br>
2024-12-01 18:36:19 -0300

    atividade

--- atividade 11/label_imagem.py
+++ atividade 11/label_imagem.py
@@ -7,7 +7,7 @@ info = "esse texto será\nexibido no rótulo\nem varias linhas."
 rotulo = Label(janela, text=info, justify="left")
 rotulo.grid(row=0, column=0)
 
-logo = PhotoImage(file-"logo.png")
+logo = PhotoImage(file="logo.png")
 rotulo2 = Label(janela, image = logo)
 rotulo2.grid(row=0, column=1)
 
